84bbcf44d54b9a153619608cfadb723f4ced6282
Yuri <dog-pit10@yandex.ru>
2020-10-20 13:46:07 +0300

    add modifi file  js

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,26 @@
-$('#menu_links').on('click touchstart', menu);
-$('#button_krest').on('click touchstart', buttonKrest);
+// $('#menu_links').on('click touchstart', menu);
+// $('#button_krest').on('click touchstart', buttonKrest);
 
-function menu() {
-    $('#menu_links').css('display', 'none');
-    $('#menu_a').css('display', 'flex');   
+
+// function menu() {
+//     $('#menu_links').css('display', 'none');
+//     $('#menu_a').css('display', 'flex');   
+// }
+
+// function buttonKrest() {
+//     $('#menu_a').css('display', 'none');
+//     $('#menu_links').css('display', 'contents');
+// }
+
+button_krest.style.display = 'none';
+
+document.getElementById('menu_links').onclick = function menu (){
+    menu_links.style.display = 'none';
+    menu_a.style.display = 'flex';
+    button_krest.style.display = 'flex';
 }
-
-function buttonKrest() {
-    $('#menu_a').css('display', 'none');
-    $('#menu_links').css('display', 'contents');
+document.getElementById('button_krest').onclick = function kross (){
+    menu_a.style.display = 'none';
+    menu_links.style.display = 'contents';
+    button_krest.style.display = 'none';
 }
-
