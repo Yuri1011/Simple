0c552da05af39fca8060e2df0035ebbf374c9b5d
Yuri <dog-pit10@yandex.ru>
2020-10-21 15:19:54 +0300

    add modifi file css and file .button_krest add color and size

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,4 @@
-// $('#menu_links').on('click touchstart', menu);
-// $('#button_krest').on('click touchstart', buttonKrest);
-
-
-// function menu() {
-//     $('#menu_links').css('display', 'none');
-//     $('#menu_a').css('display', 'flex');   
-// }
-
-// function buttonKrest() {
-//     $('#menu_a').css('display', 'none');
-//     $('#menu_links').css('display', 'contents');
-// }
-
-
+'use strict'
 
 button_krest.style.display = 'none';
 
