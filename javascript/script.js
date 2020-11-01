6ead903b9155e62893a4f3054d2b72fa7518c338
Yuri <dog-pit10@yandex.ru>
2020-11-02 00:39:10 +0300

    add modified js file

--- javascript/script.js
+++ javascript/script.js
@@ -1,69 +1,65 @@
-'use strict'
+"use strict";
 
-button_krest.style.display = 'none';
+button_krest.style.display = "none";
 
-document.getElementById('menu_links').onclick = function menu (){
-    menu_links.style.display = 'none';
-    menu_a.style.display = 'flex';
-    button_krest.style.display = 'flex';
-}
-document.getElementById('button_krest').onclick = function kross (){
-    menu_a.style.display = 'none';
-    menu_links.style.display = 'contents';
-    button_krest.style.display = 'none';
-} 
+document.getElementById("menu_links").onclick = () => {
+	menu_links.style.display = "none";
+	menu_a.style.display = "flex";
+	button_krest.style.display = "flex";
+};
+document.getElementById("button_krest").onclick = () => {
+	menu_a.style.display = "none";
+	menu_links.style.display = "contents";
+	button_krest.style.display = "none";
+};
 
-document.getElementById('home').onclick = function () {
-    document.querySelector('#home');
-    home.classList.add('active');
-    cont.classList.remove('active');
-    abot.classList.remove('active');
-    serv.classList.remove('active');
-    port.classList.remove('active');
-    test.classList.remove('active');
-}
-document.getElementById('abot').onclick = function () {
-    document.querySelector('#abot');
-    abot.classList.add('active');
-    home.classList.remove('active');
-    cont.classList.remove('active');
-    serv.classList.remove('active');
-    port.classList.remove('active');
-    test.classList.remove('active');
-}
-document.getElementById('serv').onclick = function () {
-    document.querySelector('#serv');
-    serv.classList.add('active');
-    home.classList.remove('active');
-    abot.classList.remove('active');
-    cont.classList.remove('active');
-    port.classList.remove('active');
-    test.classList.remove('active');
-}
-document.getElementById('port').onclick = function () {
-    document.querySelector('#port');
-    port.classList.add('active');
-    home.classList.remove('active');
-    abot.classList.remove('active');
-    serv.classList.remove('active');
-    cont.classList.remove('active');
-    test.classList.remove('active');
-}
-document.getElementById('test').onclick = function () {
-    document.querySelector('#test');
-    test.classList.add('active');
-    home.classList.remove('active');
-    abot.classList.remove('active');
-    serv.classList.remove('active');
-    port.classList.remove('active');
-    cont.classList.remove('active');
-}
-document.getElementById('cont').onclick = function () {
-    document.querySelector('#cont');
-    cont.classList.add('active');
-    home.classList.remove('active');
-    abot.classList.remove('active');
-    serv.classList.remove('active');
-    port.classList.remove('active');
-    test.classList.remove('active');
-}
+document.getElementById("home").onclick = () => {
+
+	home.classList.add("active");
+	cont.classList.remove("active");
+	abot.classList.remove("active");
+	serv.classList.remove("active");
+	port.classList.remove("active");
+	test.classList.remove("active");
+};
+document.getElementById("abot").onclick = () => {
+	abot.classList.add("active");
+	home.classList.remove("active");
+	cont.classList.remove("active");
+	serv.classList.remove("active");
+	port.classList.remove("active");
+	test.classList.remove("active");
+};
+document.getElementById("serv").onclick = () => {
+	serv.classList.add("active");
+	home.classList.remove("active");
+	abot.classList.remove("active");
+	cont.classList.remove("active");
+	port.classList.remove("active");
+	test.classList.remove("active");
+};
+document.getElementById("port").onclick = () => {
+	port.classList.add("active");
+	home.classList.remove("active");
+	abot.classList.remove("active");
+	serv.classList.remove("active");
+	cont.classList.remove("active");
+	test.classList.remove("active");
+};
+document.getElementById("test").onclick = () => {
+	test.classList.add("active");
+	home.classList.remove("active");
+	abot.classList.remove("active");
+	serv.classList.remove("active");
+	port.classList.remove("active");
+	cont.classList.remove("active");
+};
+document.getElementById("cont").onclick = () => {
+	cont.classList.add("active");
+	home.classList.remove("active");
+	abot.classList.remove("active");
+	serv.classList.remove("active");
+	port.classList.remove("active");
+	test.classList.remove("active");
+};
+
